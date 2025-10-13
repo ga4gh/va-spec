4f34251e0eaf1dd912bfa9c01fe046bfb76bcf9d
Kori Kuzma <korikuzma@gmail.com>
2025-10-13 14:23:18 -0400

    wip: uncomment tests

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -35,7 +35,7 @@ def test_trial_use_class_coverage():
         tested_classes.add(test_cls_name)
 
     print(trial_use_classes - tested_classes - va_abstract_classes)
-    #assert len(trial_use_classes - tested_classes - va_abstract_classes) == 0
+    assert len(trial_use_classes - tested_classes - va_abstract_classes) == 0
 
 def test_trial_use_property_coverage():
     trial_use_classes = _get_trial_use_classes()
@@ -66,5 +66,5 @@ def test_trial_use_property_coverage():
             elif covered is False:
                 no_coverage_properties.add(f'{tu_class}.{tu_class_property}')
 
-    # assert(len(no_coverage_properties) == 0), \
-    #    f"The following properties lack test coverage: {no_coverage_properties}"
+    assert(len(no_coverage_properties) == 0), \
+       f"The following properties lack test coverage: {no_coverage_properties}"
